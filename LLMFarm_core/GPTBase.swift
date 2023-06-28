aff728870a94f6ab6907af43ed3789abd5477f5c
guinmoon <guinmoon@gmail.com>
2023-06-28 20:26:56 +0300

    0.1.5 add replit and gpt2 inference Changelog: - [ ] add: gpt2 inference - [ ] add: replit inference - [ ] some work for metal

---
+++ LLMFarm_core/GPTBase.swift
@@ -0,0 +1,8 @@
+//
+//  GPTBase.swift
+//  LLMFarm
+//
+//  Created by guinmoon on 23.06.2023.
+//
+
+import Foundation
---
+++ LLMFarm_core/Replit.swift
@@ -0,0 +1,8 @@
+//
+//  Replit.swift
+//  LLMFarm
+//
+//  Created by guinmoon on 25.06.2023.
+//
+
+import Foundation

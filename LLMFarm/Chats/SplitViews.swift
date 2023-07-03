0f049b8f576fe8854575f2ceeae768b3a057c6a2
guinmoon <guinmoon@gmail.com>
2023-07-03 19:08:33 +0300

    UI Improvements

---
+++ LLMFarm/Chats/SplitViews.swift
@@ -0,0 +1,8 @@
+//
+//  SplitViews.swift
+//  LLMFarm
+//
+//  Created by guinmoon on 03.07.2023.
+//
+
+import Foundation
---
+++ LLMFarm/Orientation_helper.swift
@@ -0,0 +1,8 @@
+//
+//  Orientation_helper.swift
+//  LLMFarm
+//
+//  Created by guinmoon on 03.07.2023.
+//
+
+import Foundation

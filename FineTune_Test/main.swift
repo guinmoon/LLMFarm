fffee4379c69c99a0959a6012b98639934491697
guinmoon <guinmoon@gmail.com>
2023-10-30 20:59:32 +0300

    finetune prepare

---
+++ FineTune_Test/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  FineTune_Test
+//
+//  Created by guinmoon on 30.10.2023.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ LLMFarm/Tune/FineTune.swift
@@ -0,0 +1,8 @@
+//
+//  FineTune.swift
+//  LLMFarm
+//
+//  Created by guinmoon on 26.10.2023.
+//
+
+import Foundation

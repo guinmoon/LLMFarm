70022d238514692478b37738692bb5fb0f7ff288
guinmoon <guinmoon@gmail.com>
2023-11-06 20:29:37 +0300

    FineTune view

--- LLMFarm/Tune/FineTuneView.swift
+++ LLMFarm/Tune/FineTuneView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 
 struct FineTuneView: View {
+    @EnvironmentObject var aiChatModel: AIChatModel
     @State private var isModelImporting: Bool = false
     @State private var isDataSetImporting: Bool = false
     @State var models_previews = get_models_list()!
@@ -18,9 +19,17 @@ struct FineTuneView: View {
     @State private var dataset_file_url: URL = URL(filePath: "/")
     @State private var dataset_file_path: String = "Select dataset"
     @State private var lora_name: String = ""
+    @State private var n_ctx: Int32 = 64
+    @State private var n_batch: Int32 = 4
+    @State private var adam_iter: Int32 = 30
+    @State private var n_threads: Int32 = 0
+    @State private var use_metal: Bool = false
+    @State private var use_checkpointing: Bool = false
+    @State private var tune_log: String = ""
+    
     
     var body: some View {
-        VStack{
+        VStack(alignment: .leading){
             HStack {
                 Menu {
                     Button {
@@ -38,7 +47,7 @@ struct FineTuneView: View {
                             Button(model["file_name"]!){
                                 //                                            model_file_name = model["file_name"]!
                                 model_file_path = model["file_name"]!
-                                lora_name = get_file_name_without_ext(fileName:model_file_path) + ".bin"
+                                lora_name = get_file_name_without_ext(fileName:model_file_path) + "_" + get_file_name_without_ext(fileName:dataset_file_path) + ".bin"
                             }
                         }
                     }
@@ -62,7 +71,7 @@ struct FineTuneView: View {
                     //#else
                     //                                    model_file_path = selectedFile.path
                     //#endif
-                    lora_name = get_file_name_without_ext(fileName:selectedFile.lastPathComponent) + ".bin"
+                    lora_name = get_file_name_without_ext(fileName:model_file_path) + "_" + get_file_name_without_ext(fileName:dataset_file_path) + ".bin"
                 } catch {
                     // Handle failure.
                     print("Unable to read file contents")
@@ -72,7 +81,7 @@ struct FineTuneView: View {
             
             
             HStack {
-
+                
                 Menu {
                     Button {
                         Task {
@@ -87,9 +96,9 @@ struct FineTuneView: View {
                     Section("Avalible adapters") {
                         ForEach(datasets_preview, id: \.self) { model in
                             Button(model["file_name"]!){
-//                                            model_file_name = model["file_name"]!
+                                //                                            model_file_name = model["file_name"]!
                                 dataset_file_path = model["file_name"]!
-//                                lora_title = get_file_name_without_ext(fileName:lora_file_path)
+                                lora_name = get_file_name_without_ext(fileName:model_file_path) + "_" + get_file_name_without_ext(fileName:dataset_file_path) + ".bin"
                             }
                         }
                     }
@@ -106,7 +115,7 @@ struct FineTuneView: View {
                     guard let selectedFile: URL = try result.get().first else { return }
                     dataset_file_url = selectedFile
                     dataset_file_path = selectedFile.lastPathComponent
-//                    lora_title = get_file_name_without_ext(fileName:selectedFile.lastPathComponent)
+                    lora_name = get_file_name_without_ext(fileName:model_file_path) + "_" + get_file_name_without_ext(fileName:dataset_file_path) + ".bin"
                 } catch {
                     print("Unable to add file")
                     print(error.localizedDescription)
@@ -119,13 +128,101 @@ struct FineTuneView: View {
                 DidEndEditingTextField(text: $lora_name,didEndEditing: { newName in})
                     .frame(maxWidth: .infinity, alignment: .leading)
 #else
-                TextField("Title...", text: $lora_name)
+                TextField("LoRA file name...", text: $lora_name)
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .textFieldStyle(.plain)
 #endif
                 
             }
             .padding()
+            
+            
+            HStack {
+                Text("Threads:")
+                    .frame(maxWidth: 75, alignment: .leading)
+                TextField("count..", value: $n_threads, format:.number)
+                    .frame( alignment: .leading)
+                    .multilineTextAlignment(.trailing)
+                    .textFieldStyle(.plain)
+#if os(iOS)
+                    .keyboardType(.numberPad)
+#endif
+            }
+            .padding(.horizontal)
+            .padding(.top, 5)
+            
+            HStack {
+                Toggle("Metal", isOn: $use_metal)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .padding(.horizontal)
+            .padding(.bottom, 4)
+            
+            HStack {
+                Toggle("Use Checkpointing", isOn: $use_checkpointing)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .padding(.horizontal)
+            .padding(.bottom, 4)
+            
+            HStack {
+                Text("Adam Iter:")
+                    .frame(maxWidth: 95, alignment: .leading)
+                TextField("count..", value: $adam_iter, format:.number)
+                    .frame( alignment: .leading)
+                    .multilineTextAlignment(.trailing)
+                    .textFieldStyle(.plain)
+#if os(iOS)
+                    .keyboardType(.numberPad)
+#endif
+            }
+            .padding(.horizontal)
+            
+            HStack {
+                Text("Context:")
+                    .frame(maxWidth: 75, alignment: .leading)
+                TextField("size..", value: $n_ctx, format:.number)
+                    .frame( alignment: .leading)
+                    .multilineTextAlignment(.trailing)
+                    .textFieldStyle(.plain)
+#if os(iOS)
+                    .keyboardType(.numberPad)
+#endif
+            }
+            .padding(.horizontal)
+            
+            HStack {
+                Text("N_Batch:")
+                    .frame(maxWidth: 75, alignment: .leading)
+                TextField("size..", value: $n_batch, format:.number)
+                    .frame( alignment: .leading)
+                    .multilineTextAlignment(.trailing)
+                    .textFieldStyle(.plain)
+#if os(iOS)
+                    .keyboardType(.numberPad)
+#endif
+            }
+            .padding(.horizontal)
+            
+            VStack {
+                Text("Log:")
+                    .frame(maxWidth: 75, alignment: .leading)
+                TextEditor(text:$tune_log).frame(minHeight: 5)
+            }
+            .padding(.horizontal)
+            
+            HStack{
+                Button {
+                    Task {
+                        
+                    }
+                } label: {
+                    Text("Run finetune")
+                }
+            }
+            .padding()
+            .frame(maxWidth: .infinity,alignment: .trailing)
+            
         }
         .frame(maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,alignment:.topLeading)
         .navigationTitle("FineTune")

b5f6a2ffa071b7a09699b3211e47fa64b95e6291
guinmoon <guinmoon@gmail.com>
2023-12-02 19:07:00 +0300

    finetune updated to b1601

--- FineTune_Test/main.swift
+++ FineTune_Test/main.swift
@@ -9,12 +9,16 @@ import Foundation
 import llmfarm_core
 
 
-var open_llama_finetune: LLaMa_FineTune = LLaMa_FineTune("/Users/guinmoon/dev/alpaca_llama_etc/openllama-3b-v2-q8_0.gguf",
+var open_llama_finetune: LLaMa_FineTune = LLaMa_FineTune("/Users/guinmoon/dev/alpaca_llama_etc/open_llama_3b_v2_ggml-model-Q8_0.gguf",
                                         "/Users/guinmoon/dev/alpaca_llama_etc/lora-open-llama-3b-v2-q8_0-shakespeare-LLMFarm.bin",
                                         "/Users/guinmoon/dev/alpaca_llama_etc/pdf/shakespeare.txt")
 
-
-try? open_llama_finetune.finetune()
+open_llama_finetune.use_metal = true
+open_llama_finetune.threads = 12
+try? open_llama_finetune.finetune({
+    str in
+    print(str)
+})
 
 
 
